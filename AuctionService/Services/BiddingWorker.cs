0a79ec147ac8a1440312547d73d5808f159fc1a7
Gabrieldarri <gabrielbjarnason@gmail.com>
2025-05-28 10:30:16 +0200

    admin admin?

--- AuctionService/Services/BiddingWorker.cs
+++ AuctionService/Services/BiddingWorker.cs
@@ -19,6 +19,8 @@ public class BiddingWorker : BackgroundService
         {
             HostName = "rabbitmq",
             Port = 5672,
+            UserName = "admin",
+            Password = "admin"
         };
     }
 
